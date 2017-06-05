da697c5cf7004aba753ca4c33d3baff85b860576
Pamela Toman <ptoman@stanford.edu>
2017-06-05 00:51:55 -0700

    Convert additive noise into function. Still outside the data processing of model.

--- code/utils/dataAugmentation_additive.py
+++ code/utils/dataAugmentation_additive.py
@@ -18,78 +18,78 @@ import matplotlib.pyplot as plt
 # https://sound.stackexchange.com/questions/25453/a-mathematical-formula-describing-mains-hum
 
 
+
+def add_noise(emg_signal, show_plot=True):
+    # Sampling
+    # 1 second of data requires 600 frames.  And 600 fps is 600 Hz, sampling rate of EMG.
+    fs = 600 
+    Ts = 1/fs
+
+    # Time vector
+    t = np.arange(0, len(emg_signal)/fs, Ts) # each unit of t is a second
+
+    # Noise
+    randAmplitudeScale = np.random.random()*0.1
+    randOffset = t[np.random.randint(0, fs)]
+
+    fNoise = 60;                                           # Frequency [Hz]
+    aNoise = randAmplitudeScale*abs(np.max(emg_signal))    # Amplitude
+    noise  = aNoise * np.sin(2 * np.pi * t * fNoise + randOffset)
+
+    # Signal + Noise
+    signalNoise = emg_signal + noise
+    
+    # Plot an example of augmentation
+    DATA_XRANGE = list(range(200))
+    DATA_YRANGE = (-32768, 32767) # 2-byte signed integer, as per corpus spec
+
+    plt.subplots(nrows=3, ncols=1)
+    plt.tight_layout()
+
+    p = plt.subplot(3, 1, 1)
+    plt.title("Data Augmentation: Additive Noise")
+    plt.plot(t[DATA_XRANGE], signal[DATA_XRANGE]) 
+    plt.ylim(DATA_YRANGE)
+    plt.text(.5,.98,'EMG Signal',
+        horizontalalignment='center',
+        verticalalignment='top',
+        transform=plt.gca().transAxes)
+    plt.ylabel("Amplitude")
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+
+    plt.subplot(3, 1, 2)
+    plt.plot(t[DATA_XRANGE], noise[DATA_XRANGE])
+    plt.ylim(DATA_YRANGE)
+    plt.text(.5,.98,"60 Hz Noise\n(Amplitude of %4.2fx Signal Range, Offset of %4.2f sec)" %
+        (randAmplitudeScale, randOffset),
+        horizontalalignment='center',
+        verticalalignment='top',
+        transform=plt.gca().transAxes)
+    plt.ylabel("Amplitude")
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+
+    plt.subplot(3, 1, 3)
+    plt.plot(t[DATA_XRANGE], signalNoise[DATA_XRANGE])
+    plt.ylim(DATA_YRANGE)
+    plt.text(.5,.98,'Noisy Signal',
+        horizontalalignment='center',
+        verticalalignment='top',
+        transform=plt.gca().transAxes)
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Amplitude")
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+        
+    plt.show()
+
+    return signalNoise
+
+
 # Get some data to play with
 with open("data/train/002_001_0100.pkl", "rb") as f:
     audio, emg = pickle.load(f)
 
 
-# 1 second of data requires 600 frames.  And 600 fps is 600 Hz, sampling rate of EMG.
-
-# Sampling
-fs = 600; 
-Ts = 1/fs;
-
-# Signal
 signal = emg["emg1"]
-
-# Time vector
-t = np.arange(0, len(signal)/fs, Ts) # each unit of t is a second
-
-# Noise
-randAmplitudeScale = np.random.random()*0.1
-randOffset = t[np.random.randint(0, fs)]
-
-fNoise = 60;                                       # Frequency [Hz]
-aNoise = randAmplitudeScale*abs(np.max(signal));   # Amplitude
-noise  = aNoise * np.sin(2 * np.pi * t * fNoise + randOffset);
-
-# Signal + Noise
-signalNoise = signal + noise;
-
-
-
-
-# Plot an example of augmentation
-
-DATA_XRANGE = list(range(200))
-DATA_YRANGE = (-32768, 32767) # 2-byte signed integer, as per corpus spec
-
-plt.subplots(nrows=3, ncols=1)
-plt.tight_layout()
-
-p = plt.subplot(3, 1, 1)
-plt.title("Data Augmentation: Additive Noise")
-plt.plot(t[DATA_XRANGE], signal[DATA_XRANGE]) 
-plt.ylim(DATA_YRANGE)
-plt.text(.5,.98,'EMG Signal',
-    horizontalalignment='center',
-    verticalalignment='top',
-    transform=plt.gca().transAxes)
-plt.ylabel("Amplitude")
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
-
-plt.subplot(3, 1, 2)
-plt.plot(t[DATA_XRANGE], noise[DATA_XRANGE])
-plt.ylim(DATA_YRANGE)
-plt.text(.5,.98,"60 Hz Noise\n(Amplitude of %4.2fx Signal Range, Offset of %4.2f sec)" %
-    (randAmplitudeScale, randOffset),
-    horizontalalignment='center',
-    verticalalignment='top',
-    transform=plt.gca().transAxes)
-plt.ylabel("Amplitude")
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
-
-plt.subplot(3, 1, 3)
-plt.plot(t[DATA_XRANGE], signalNoise[DATA_XRANGE])
-plt.ylim(DATA_YRANGE)
-plt.text(.5,.98,'Noisy Signal',
-    horizontalalignment='center',
-    verticalalignment='top',
-    transform=plt.gca().transAxes)
-plt.xlabel("Time (seconds)")
-plt.ylabel("Amplitude")
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
-    
-plt.show()
+signalNoise = add_noise(signal)
 
 
